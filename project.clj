cbf15996641c3711f665d9c1d47e3cee6d8aec7d
Matt Oquist <moquist@majen.net>
2015-02-25 00:15:06 -0500

    Add new dependencies, ring handling, src paths.
    These dependencies cover the back end (http-kit, ring, compojure) and the
    addition of Clojure source (src/clj).

--- project.clj
+++ project.clj
@@ -9,18 +9,26 @@
                  [figwheel "0.2.5-SNAPSHOT"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                  [sablono "0.3.4"]
-                 [org.omcljs/om "0.8.8"]]
+                 [org.omcljs/om "0.8.8"]
+                 [http-kit "2.1.18"]
+                 [ring "1.3.2"]
+                 [compojure "1.3.1"]
+                 [com.cemerick/url "0.1.1"]
+                 [cheshire "5.4.0"]]
 
   :plugins [[lein-cljsbuild "1.0.4"]
-            [lein-figwheel "0.2.5-SNAPSHOT"]]
+            [lein-figwheel "0.2.5-SNAPSHOT"]
+            [lein-ring "0.9.2"]]
 
-  :source-paths ["src"]
+  :ring {:handler om-my.back-end/handler}
+
+  :source-paths ["src/clj" "src/cljs"]
 
   :clean-targets ^{:protect false} ["resources/public/js/compiled"]
 
   :cljsbuild {
     :builds [{:id "dev"
-              :source-paths ["src" "dev_src"]
+              :source-paths ["src/cljs" "dev_src"]
               :compiler {:output-to "resources/public/js/compiled/om_my.js"
                          :output-dir "resources/public/js/compiled/out"
                          :optimizations :none
@@ -30,7 +38,7 @@
                          :source-map-timestamp true
                          :cache-analysis true }}
              {:id "min"
-              :source-paths ["src"]
+              :source-paths ["src/cljs"]
               :compiler {:output-to "resources/public/js/compiled/om_my.js"
                          :main om-my.core
                          :optimizations :advanced
@@ -51,7 +59,7 @@
              ;; if you want to embed a ring handler into the figwheel http-kit
              ;; server, this is simple ring servers, if this
              ;; doesn't work for you just run your own server :)
-             ;; :ring-handler hello_world.server/handler
+             :ring-handler om-my.back-end/handler
 
              ;; To be able to open files in your editor from the heads up display
              ;; you will need to put a script on your path.

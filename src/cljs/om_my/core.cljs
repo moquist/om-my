dfab89367b553cd8ea8d561e7f9151d5fc80c95f
Matt Oquist <moquist@majen.net>
2015-02-25 00:17:50 -0500

    Add simple handling of one basic query.

--- src/cljs/om_my/core.cljs
+++ src/cljs/om_my/core.cljs
@@ -1,6 +1,8 @@
 (ns ^:figwheel-always om-my.core
-    (:require[om.core :as om :include-macros true]
-              [om.dom :as dom :include-macros true]))
+    (:require [om.core :as om :include-macros true]
+              [om.dom :as dom :include-macros true]
+              [om-my.utils :as utils]
+              [cemerick.url :refer [url-encode]]))
 
 (enable-console-print!)
 
@@ -10,11 +12,32 @@
 
 (defonce app-state (atom {:text "Hello world!"}))
 
+(defn get-movie [title]
+  (let [title (url-encode title)]
+    (utils/edn-xhr
+     {:method :post
+      :url "/rt"
+      :data (str "movies.json?q=" title)
+      :on-complete (fn [response] (reset! app-state {:movie response}))})))
+
+(defn display-list [items]
+  (apply dom/ul nil
+         (map #(dom/li nil %) items)))
+
+(defn display-movie-abridged-cast [movie]
+  (let [cast (-> movie
+                 :movies
+                 first
+                 :abridged_cast)]
+    (display-list (map :name cast))))
+
 (om/root
   (fn [data owner]
     (reify om/IRender
       (render [_]
-        (dom/h1 nil (:text data)))))
+        (cond
+         (:text data) (dom/h1 nil (:text data))
+         (:movie data) (display-movie-abridged-cast (:movie data))))))
   app-state
   {:target (. js/document (getElementById "app"))})
 

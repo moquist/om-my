6e88ccfacf111532441b0812af1887a5b3af8a1e
Matt Oquist <moquist@majen.net>
2015-02-25 00:14:41 -0500

    Remove old debugging 'println.

--- dev_src/om_my/dev.clj
+++ dev_src/om_my/dev.clj
@@ -1,7 +1,6 @@
 (ns om-my.dev)
 
 (defmacro websocket-url []
-  (println :making-websocket-url)
   (if-let [port (Long/parseLong (System/getenv "LEIN_FIGWHEEL_PORT"))]
     (str "ws://localhost:" port  "/figwheel-ws")
     (throw (Exception. "Missing LEIN_FIGWHEEL_PORT environment variable. Set it."))))
